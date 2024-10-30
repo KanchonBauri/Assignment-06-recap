7c8a8bf2c23464faf6c91c2ffc557af9791dff71
KanchonBauri <kanchonbauri1998@gmail.com>
2024-10-30 09:02:53 +0600

    btn categories display

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const loadCategories = async () => {
-    const res = await fetch (
+    const res = await fetch(
         'https://openapi.programming-hero.com/api/peddy/categories'
     )
     const data = await res.json()
     displayCategories(data.categories)
 }
 
+
 const displayCategories = data => {
-    console.log(data)
+    // console.log(data)
+    const categoryContainer = document.getElementById('pet-categories')
+    data.forEach(category => {
+        // console.log(category)
+        const div = document.createElement('div')
+        div.innerHTML = `
+<button class="btn category-btn bg-white flex items-center rounded-xl border px-14 py-4 gap-4 cursor-pointer h-full " >
+   <img class="w-10" src="${category.category_icon}" alt="">
+   <p class="text-xl font-bold">${category.category}</p>
+</button>
+`
+categoryContainer.appendChild(div);
+    })
 }
 
 
