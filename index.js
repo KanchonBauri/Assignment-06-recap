e30f595dfb2db2ea69064143ecd21f4c548aa077
KanchonBauri <kanchonbauri1998@gmail.com>
2024-10-30 08:42:15 +0600

    btn categories data load

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console,log("helllloo")
+const loadCategories = async () => {
+    const res = await fetch (
+        'https://openapi.programming-hero.com/api/peddy/categories'
+    )
+    const data = await res.json()
+    displayCategories(data.categories)
+}
+
+const displayCategories = data => {
+    console.log(data)
+}
+
+
+
+loadCategories();
